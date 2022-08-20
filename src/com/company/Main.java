cf4ebf73a21001e6ba53704ff11eed84b12c3712
Papyoz <111503973+Papyoz@users.noreply.github.com>
2022-08-20 14:32:18 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,2 +1,35 @@
-package com.company;public class Main {
+package com.company;
+
+import java.util.Scanner;
+
+/*
+%d - Целочисленные 1,2,3...
+%f - float
+%.3f - округление
+%c - char
+%s - string
+%% - проценты
+ */
+
+public class Main {
+    public static void main(String[] args){
+        double a,b;
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("a = "); a = in.nextDouble();
+        System.out.print("b = "); b = in.nextDouble();
+
+        double p = 2*(a+b);
+        System.out.printf("%.2f",p);
+        in.close();
+
+        /*
+        next - До пробела
+        nextLine - до перевода строки
+        nextByte - считывает как byte
+        nextShort - short...
+        ...
+         */
+
+    }
 }
